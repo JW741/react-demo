85261aa84d584afb479725c086d945aa9230a963
JW741 <149878597+JW741@users.noreply.github.com>
2023-11-06 14:13:17 +0800

    Update request.js
    JW

--- src/utils/request.js
+++ src/utils/request.js
@@ -37,44 +37,6 @@ request.interceptors.response.use(
       clearToken()
       router.navigate('/login')
       window.location.reload()
-    }
-    try{
-      let msg = ''
-      switch (error.response.status) {
-        case 400:
-          msg = '请求错误'
-          break
-        case 403:
-          msg = '拒绝访问'
-          break
-        case 404:
-          msg = '请求地址出错'
-          break
-        case 408:
-          msg = '请求超时'
-          break
-        case 500:
-          msg = '服务器内部错误'
-          break
-        case 501:
-          msg = '服务未实现'
-          break
-        case 502:
-          msg = '网关错误'
-          break
-        case 504:
-          msg = '网关超时'
-          break
-        case 505:
-          msg = 'HTTP版本不受支持'
-          break
-        default:
-          msg = '请稍后访问'
-      }
-      message.error(msg)
-    }catch(err){
-      
-    }
     
     return Promise.reject(error)
   }
